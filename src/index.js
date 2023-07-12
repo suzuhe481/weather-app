80e244e835a45bd86a2c25415b504a4b92b11cab
Hector <baseballhector@gmail.com>
2023-07-12 14:48:38 -0400

    Imported reset.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import weatherForm from "./components/weatherForm/UI/weatherForm";
 
+import "./reset.css";
+
 const weatherFormContainer = document.createElement("div");
 weatherFormContainer.id = "weather-form-container";
 document.body.appendChild(weatherFormContainer);
