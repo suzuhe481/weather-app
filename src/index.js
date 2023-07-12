8a5aa8c2f956dab9e7480952752e0a44934eace9
Hector <baseballhector@gmail.com>
2023-07-11 21:55:05 -0400

    Displays the form and an empty container for weather cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import weatherCard from "./components/weatherCard/UI/weatherCard";
+import weatherForm from "./components/weatherForm/UI/weatherForm";
 
-document.body.appendChild(weatherCard());
+const weatherFormContainer = document.createElement("div");
+weatherFormContainer.id = "weather-form-container";
+document.body.appendChild(weatherFormContainer);
+
+weatherFormContainer.appendChild(weatherForm());
+
+const weatherCardsContainer = document.createElement("div");
+weatherCardsContainer.id = "weather-cards-container";
+document.body.appendChild(weatherCardsContainer);
