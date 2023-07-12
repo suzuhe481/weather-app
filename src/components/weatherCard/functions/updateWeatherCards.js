78bebc0d26d7ed3f960ba9367e515f55f4ee091f
Hector <baseballhector@gmail.com>
2023-07-12 17:55:46 -0400

    Calls getWeather and to create multiple weather cards

--- src/components/weatherCard/functions/updateWeatherCards.js
+++ src/components/weatherCard/functions/updateWeatherCards.js
@@ -1,17 +1,35 @@
+import weatherForm from "../../weatherForm/UI/weatherForm";
 import weatherCard from "../UI/weatherCard";
+import getWeather from "./getWeather";
 
 const updateWeatherCards = (event) => {
   event.preventDefault();
 
-  const card = weatherCard();
+  var card;
 
   const weatherCardsContainer = document.getElementById(
     "weather-cards-container"
   );
-
   weatherCardsContainer.innerHTML = "";
 
-  weatherCardsContainer.appendChild(card);
+  getWeather()
+    .then((weatherData) => {
+      // Creates cards for each forecast day.
+      // Index is passed to know which is the current day, at i == 0.
+      for (var i = 0; i < weatherData.forecast.length; i++) {
+        card = weatherCard(weatherData, i);
+        weatherCardsContainer.appendChild(card);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+
+      const errorMessage = document.createElement("div");
+      errorMessage.innerHTML = "Weather data could not be displayed.";
+      card = errorMessage;
+
+      weatherCardsContainer.appendChild(card);
+    });
 };
 
 export default updateWeatherCards;
