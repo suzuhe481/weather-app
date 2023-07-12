7242826075cb28b2ac611ee40a6b007bbc64874a
Hector <baseballhector@gmail.com>
2023-07-12 13:36:09 -0400

    Removed label and added placeholder to input. Imported stylesheet

--- src/components/weatherForm/UI/weatherForm.js
+++ src/components/weatherForm/UI/weatherForm.js
@@ -1,22 +1,22 @@
 import updateWeatherCards from "../../weatherCard/functions/updateWeatherCards";
 
+import "./weatherForm.css";
+
 // Outputs a form that collects a location from the user.
 const weatherForm = () => {
   const formContainer = document.createElement("div");
 
   const weatherForm = document.createElement("form");
 
-  const locationLabel = document.createElement("label");
-  locationLabel.innerHTML = "Location: ";
-
   const locationInput = document.createElement("input");
   locationInput.id = "location-input";
+  locationInput.placeholder = "Location...";
 
   const submitButton = document.createElement("button");
+  submitButton.id = "location-submit";
   submitButton.innerHTML = "Submit";
   submitButton.onclick = updateWeatherCards;
 
-  weatherForm.appendChild(locationLabel);
   weatherForm.appendChild(locationInput);
   weatherForm.appendChild(submitButton);
 
