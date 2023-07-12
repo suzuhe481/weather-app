98899ce0956af00323fb2e652292749a7a53cdbc
Hector <baseballhector@gmail.com>
2023-07-11 21:55:39 -0400

    Submit button calls function to update weather cards

--- src/components/weatherForm/UI/weatherForm.js
+++ src/components/weatherForm/UI/weatherForm.js
@@ -1,3 +1,5 @@
+import updateWeatherCards from "../../weatherCard/functions/updateWeatherCards";
+
 // Outputs a form that collects a location from the user.
 const weatherForm = () => {
   const formContainer = document.createElement("div");
@@ -8,9 +10,11 @@ const weatherForm = () => {
   locationLabel.innerHTML = "Location: ";
 
   const locationInput = document.createElement("input");
+  locationInput.id = "location-input";
 
   const submitButton = document.createElement("button");
   submitButton.innerHTML = "Submit";
+  submitButton.onclick = updateWeatherCards;
 
   weatherForm.appendChild(locationLabel);
   weatherForm.appendChild(locationInput);
