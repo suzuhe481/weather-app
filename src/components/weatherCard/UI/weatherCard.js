637663de46645f4db85a768dbe02245cc3d6b208
Hector <baseballhector@gmail.com>
2023-07-12 21:13:55 -0400

    Refactored. Removed the weatherLocation component

--- src/components/weatherCard/UI/weatherCard.js
+++ src/components/weatherCard/UI/weatherCard.js
@@ -1,6 +1,6 @@
 import getWeather from "../functions/getWeather";
 
-import weatherLocation from "./weatherLocation";
+// import weatherLocation from "./weatherLocation";
 import weatherTemperature from "./weatherTemperature";
 import weatherWind from "./weatherWind";
 import weatherCondition from "./weatherCondition";
@@ -12,70 +12,38 @@ const weatherCard = (weatherData, i) => {
   // Adds appropriate class for a card for today's date or a future date.
   if (i === 0) {
     card.classList.add("weather-card", "current");
-
-    // Location
-    const weatherLocationContainer = document.createElement("div");
-    weatherLocationContainer.id = "weather-location-container";
-    weatherLocationContainer.appendChild(
-      weatherLocation(weatherData.forecast[i].location)
-    );
-    card.appendChild(weatherLocationContainer);
-
-    // Temperature
-    const weatherTempContainer = document.createElement("div");
-    weatherTempContainer.id = "weather-temp-container";
-    weatherTempContainer.appendChild(
-      weatherTemperature(weatherData.forecast[i].temp, i)
-    );
-    card.appendChild(weatherTempContainer);
-
-    // Wind
-    const weatherWindContainer = document.createElement("div");
-    weatherWindContainer.id = "weather-wind-container";
-    weatherWindContainer.appendChild(
-      weatherWind(weatherData.forecast[i].wind, i)
-    );
-    card.appendChild(weatherWindContainer);
-
-    // Condition
-    const weatherConditionContainer = document.createElement("div");
-    weatherConditionContainer.id = "weather-condition-container";
-    weatherConditionContainer.appendChild(
-      weatherCondition(weatherData.forecast[i].condition, i)
-    );
-    card.appendChild(weatherConditionContainer);
-
-    // Last Updated
-    const lastUpdated = document.createElement("div");
-    lastUpdated.innerHTML = weatherData.forecast[i].lastUpdated;
-    card.appendChild(lastUpdated);
   } else {
     card.classList.add("weather-card", "future");
-
-    // Temperature
-    const weatherTempContainer = document.createElement("div");
-    weatherTempContainer.id = "weather-temp-container";
-    weatherTempContainer.appendChild(
-      weatherTemperature(weatherData.forecast[i].temp, i)
-    );
-    card.appendChild(weatherTempContainer);
-
-    // Wind
-    const weatherWindContainer = document.createElement("div");
-    weatherWindContainer.id = "weather-wind-container";
-    weatherWindContainer.appendChild(
-      weatherTemperature(weatherData.forecast[i].temp, i)
-    );
-    card.appendChild(weatherWindContainer);
-
-    // Condition
-    const weatherConditionContainer = document.createElement("div");
-    weatherConditionContainer.id = "weather-condition-container";
-    weatherConditionContainer.appendChild(
-      weatherCondition(weatherData.forecast[i].condition, i)
-    );
-    card.appendChild(weatherConditionContainer);
   }
+
+  // Temperature
+  const weatherTempContainer = document.createElement("div");
+  weatherTempContainer.id = "weather-temp-container";
+  weatherTempContainer.appendChild(
+    weatherTemperature(weatherData.forecast[i].temp, i)
+  );
+  card.appendChild(weatherTempContainer);
+
+  // Wind
+  const weatherWindContainer = document.createElement("div");
+  weatherWindContainer.id = "weather-wind-container";
+  weatherWindContainer.appendChild(
+    weatherWind(weatherData.forecast[i].wind, i)
+  );
+  card.appendChild(weatherWindContainer);
+
+  // Condition
+  const weatherConditionContainer = document.createElement("div");
+  weatherConditionContainer.id = "weather-condition-container";
+  weatherConditionContainer.appendChild(
+    weatherCondition(weatherData.forecast[i].condition, i)
+  );
+  card.appendChild(weatherConditionContainer);
+
+  // Last Updated
+  const lastUpdated = document.createElement("div");
+  lastUpdated.innerHTML = weatherData.forecast[i].lastUpdated;
+  card.appendChild(lastUpdated);
 
   return card;
 };
